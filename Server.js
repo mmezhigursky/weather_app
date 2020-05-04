ac378db9f83f94cf1507a48cda6d7ed4416368e0
Max <m.mezhigurskii@gmail.com>
2020-05-04 15:46:58 +0300

    test func

--- Server.js
+++ Server.js
@@ -1,12 +1,22 @@
 // Загружаем HTTP модуль
 const port = 8000;
 const express = require('express');
-const body_parser = require('body-parser')
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('demo'));
 
 function listening(){
    console.log("server running"); 
-   console.log(`running on localhost: {$port}`);
-}
+   console.log(`running on localhost: ${port}`);
+};
 
+app.get('/', function (req, res) {
+   res.send('Hello World!');
+ });
 const server = app.listen(port, listening);
 
